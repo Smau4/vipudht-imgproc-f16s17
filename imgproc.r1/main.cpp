d8bb85c2e23b171c1763305df2ddd3f26ba6ee88
kevson <kevson@users.noreply.github.com>
2017-02-14 18:26:15 -1000

    Capture Images in a Loop

--- imgproc.r1/main.cpp
+++ imgproc.r1/main.cpp
@@ -7,41 +7,41 @@
 #include <stdarg.h>
 #include <string.h>
 #include <sys/ioctl.h>
+#include <sys/time.h>
 #include <gphoto2/gphoto2.h>
 
 #include "stdafx.h"
 #include "process_image.h"
 
 static void
-capture_to_memory(Camera *camera, GPContext *context, const char **ptr, unsigned long int *size) {
+capture_to_memory(Camera *camera, GPContext *context, const char **ptr, unsigned long int *size) 
+{
 	int retval;
 	CameraFile *file;
 	CameraFilePath camera_file_path;
 
 	printf("Capturing.\n");
-
 	/* NOP: This gets overridden in the library to /capt0000.jpg */
 	strcpy(camera_file_path.folder, "/");
 	strcpy(camera_file_path.name, "foo.jpg");
 
 	retval = gp_camera_capture(camera, GP_CAPTURE_IMAGE, &camera_file_path, context);
-	printf("  Retval: %d\n", retval);
+	printf("  gp_camera_capture: %d\n", retval);
 
 	printf("Pathname on the camera: %s/%s\n", camera_file_path.folder, camera_file_path.name);
 
 	retval = gp_file_new(&file);
-	printf("  Retval: %d\n", retval);
+	printf("  gp_file_new returned: %d\n", retval);
 	retval = gp_camera_file_get(camera, camera_file_path.folder, camera_file_path.name,
 		     GP_FILE_TYPE_NORMAL, file, context);
-	printf("  Retval: %d\n", retval);
+	printf("  gp_camera_file_get: %d\n", retval);
 
 	gp_file_get_data_and_size (file, ptr, size);
 
-	printf("Deleting.\n");
-	retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name,
-			context);
-	printf("  Retval: %d\n", retval);
-	/*gp_file_free(file); */
+	//printf("Deleting.\n");
+	//retval = gp_camera_file_delete(camera, camera_file_path.folder, camera_file_path.name, context);
+	//printf("  gp_camera_file_detect: %d\n", retval);
+	gp_file_free(file); 
 }
 
 static void
@@ -76,36 +76,40 @@ capture_to_file(Camera *camera, GPContext *context, char *fn) {
 	gp_file_free(file);
 }
 
-int
-main(int argc, char **argv) {
-	
+
+
+int main(int argc, char **argv) 
+{	
 	/* Initialize libgphoto */
 	Camera	*camera;
 	int	retval;
 	GPContext *context = sample_create_context();
 	char	*data;
 	unsigned long size;
+	struct timeval tval;
 
 	gp_camera_new(&camera);
 
-	/* When I set GP_LOG_DEBUG instead of GP_LOG_ERROR above, I noticed that the
-	 * init function seems to traverse the entire filesystem on the camera.  This
-	 * is partly why it takes so long.
-	 * (Marcus: the ptp2 driver does this by default currently.)
-	 */
 	printf("Camera init.  Takes about 10 seconds.\n");
 	retval = gp_camera_init(camera, context);
 	if (retval != GP_OK) {
-		printf("  Retval of capture_to_file: %d\n", retval);
+		printf("gp_camera_init: %d\n", retval);
 		exit (1);
 	}
-	
-	//capture_to_file(camera, context, "foo.jpg");
-	capture_to_memory(camera, context, (const char**)&data, &size);
-	
-	// image processing stuff
-	process_image(data, size);
-	
+
+	/* main loop */	
+	while (1) 
+	{	
+		// capture raw image
+		capture_to_memory(camera, context, (const char**)&data, &size);
+				
+		// process raw image / save targets to disk
+		process_image(data, size);
+
+		// wait for x seconds
+		sleep(4);
+	}	
+
 	// Cleanup
 	gp_camera_exit(camera, context);
 	gp_camera_free(camera);
--- imgproc.r1/process_image.cpp
+++ imgproc.r1/process_image.cpp
@@ -1,24 +1,47 @@
 #include "process_image.h"
-#include <stdio.h>
 #include <opencv2/opencv.hpp>
+#include <time.h>
+#include <string.h>
+#include <stdio.h>
 
 using namespace cv;
 using namespace std;
 
-void process_image(char *image, unsigned long size)
+void process_image(char *image, unsigned long image_size)
 {
-	
-	// Read image
-	Mat rawImage = Mat( 1, size, CV_8UC1, image );
-	Mat im = imdecode( rawImage, IMREAD_GRAYSCALE );
-	if ( im.data == NULL ) {
-		printf("OpenCV: Failed to read in jpg file from memory.");
-		return;
-	}
+	// filename stuff
+	char target_fn[20];
+
+	//Timers to track time program takes to run.
+	time_t start_time = clock();
+	time_t im_read_time, blob_detect_time;
+
+	//Determines scale factor of cropping.
+	static double CROP_SCALE = 1.5;
+	static double SCALING_FACTOR = 1.0;
+
+	////////////////////////////////////////////////////////////////////PREPARING IMAGES////////////////////////////////////////////////////////////////////
+
+	// Read images in color, grayscale, and inverted grayscale
+	Mat rawImage = Mat( 1, image_size, CV_8UC1, image);
+	Mat im 	     = imdecode( rawImage, IMREAD_GRAYSCALE);
 	Mat im_color = imdecode( rawImage, IMREAD_COLOR );
 	Mat im_inv;
+
+
+	//Resize the image for faster processing time.
+	Size size(1500, 2000);
+	resize(im, im, size);
+	resize(im_color, im_color, size);
+
+	//Set Inverted Greyscale image
 	bitwise_not(im, im_inv);
-	
+	im_read_time = clock();
+	printf("Took %i seconds to prepare images\n", (im_read_time - start_time));
+
+
+	////////////////////////////////////////////////////////////////////BLOB DETECTION////////////////////////////////////////////////////////////////////
+
 	// Setup SimpleBlobDetector parameters.
 	SimpleBlobDetector::Params params;
 
@@ -28,7 +51,7 @@ void process_image(char *image, unsigned long size)
 
 	// Filter by Area.
 	params.filterByArea = true;
-	params.minArea = 1200;
+	params.minArea = 600;
 
 	// Filter by Circularity
 	params.filterByCircularity = true;
@@ -42,54 +65,89 @@ void process_image(char *image, unsigned long size)
 	params.filterByInertia = true;
 	params.minInertiaRatio = 0.3;
 
-	params.minDistBetweenBlobs = 100;
+	params.minDistBetweenBlobs = 10;
 
 	// Storage for blobs
 	vector<KeyPoint> keypoints;
 	vector<KeyPoint> keypoints_inv;
 
-
-#if CV_MAJOR_VERSION < 3   // If you are using OpenCV 2
-
+#if CV_MAJOR_VERSION < 3	// If you are using OpenCV 2
 	// Set up detector with params
 	SimpleBlobDetector detector(params);
-
+	
 	// Detect blobs
 	detector.detect(im, keypoints);
-#else 
-
+#else
 	// Set up detector with params
 	Ptr<SimpleBlobDetector> detector = SimpleBlobDetector::create(params);
 
 	// Detect blobs
 	detector->detect(im, keypoints);
 	detector->detect(im_inv, keypoints_inv);
-#endif 
+#endif
+
+	blob_detect_time = clock();
+	printf("Detect blob time: %i\n", blob_detect_time - im_read_time);
 
 	// Draw detected blobs as red circles.
 	// DrawMatchesFlags::DRAW_RICH_KEYPOINTS flag ensures
 	// the size of the circle corresponds to the size of blob
 
 	Mat im_with_keypoints;
-	drawKeypoints(im, keypoints_inv, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+	drawKeypoints(im_color, keypoints, im_with_keypoints, Scalar(0, 0, 255), DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
 	namedWindow("keypoints", WINDOW_NORMAL);
-	cv::resizeWindow("keypoints", 1200, 900);
+	cv::resizeWindow("keypoints", 600, 800);
 	//Show blobs
 	imshow("keypoints", im_with_keypoints);
+
+
+	////////////////////////////////////////////////////////////////////CROPPING TARGETS////////////////////////////////////////////////////////////////////
+
+	const int numTargets = 50; // Replace with a vector later
+	cv::Mat targets[numTargets];
 	cv::Mat imageROI;
 	int i;
-	/*
+
+	//Replace with Cropping_Factor constant
+
 	for (i = 0; i < keypoints.size(); i++) {
-		imageROI = im_color(cv::Rect(keypoints[i].pt.x - keypoints[i].size / 2, keypoints[i].pt.y - keypoints[i].size / 2, keypoints[i].size, keypoints[i].size));
-		imshow("cropped_image" + i, imageROI);
+		imageROI = im_color(cv::Rect(keypoints[i].pt.x - keypoints[i].size * CROP_SCALE / 2, keypoints[i].pt.y - keypoints[i].size * CROP_SCALE / 2, keypoints[i].size * CROP_SCALE, keypoints[i].size * CROP_SCALE));
+		//imshow("cropped_image" + i, imageROI);
+		targets[i] = imageROI;
 	}
 	for (i = 0; i < keypoints_inv.size(); i++) {
-		imageROI = im_color(cv::Rect(keypoints_inv[i].pt.x - keypoints_inv[i].size / 2, keypoints_inv[i].pt.y - keypoints_inv[i].size / 2, keypoints_inv[i].size, keypoints_inv[i].size));
-		imshow("cropped_image" + (i+keypoints.size()), imageROI);
-	}*/
-	waitKey(0);
-	
-	
-	// write file to disk
-	imwrite("processed.jpg", im);
+		imageROI = im_color(cv::Rect(keypoints_inv[i].pt.x - keypoints_inv[i].size * CROP_SCALE / 2, keypoints_inv[i].pt.y - keypoints_inv[i].size * CROP_SCALE / 2, keypoints_inv[i].size * CROP_SCALE, keypoints_inv[i].size * CROP_SCALE));
+		//imshow("cropped_image" + (i + keypoints.size()), imageROI);
+		targets[i + keypoints.size()] = imageROI;
+	}
+
+	for (i = 0; i < keypoints.size() + keypoints_inv.size(); i++)
+	{
+		Mat samples(targets[i].rows * targets[i].cols, 3, CV_32F);
+		for (int y = 0; y < targets[i].rows; y++)
+			for (int x = 0; x < targets[i].cols; x++)
+				for (int z = 0; z < 3; z++)
+					samples.at<float>(y + x*targets[i].rows, z) = targets[i].at<Vec3b>(y, x)[z];
+
+		int clusterCount = 3;
+		Mat labels;
+		int attempts = 5;
+		Mat centers;
+		kmeans(samples, clusterCount, labels, TermCriteria(CV_TERMCRIT_ITER | CV_TERMCRIT_EPS, 10000, 0.0001), attempts, KMEANS_PP_CENTERS, centers);
+		Mat new_image(targets[i].size(), targets[i].type());
+		for (int y = 0; y < targets[i].rows; y++)
+			for (int x = 0; x < targets[i].cols; x++)
+			{
+				int cluster_idx = labels.at<int>(y + x*targets[i].rows, 0);
+				new_image.at<Vec3b>(y, x)[0] = centers.at<float>(cluster_idx, 0);
+				new_image.at<Vec3b>(y, x)[1] = centers.at<float>(cluster_idx, 1);
+				new_image.at<Vec3b>(y, x)[2] = centers.at<float>(cluster_idx, 2);
+			}
+
+		/* Save target image. Filename is unix time */		
+		snprintf(target_fn, sizeof(char) * 20, "%i.bmp", time(0));  
+		imwrite(target_fn, new_image);
+	}
+
+
 }
